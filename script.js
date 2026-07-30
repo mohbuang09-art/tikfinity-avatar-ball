0ecb412d7310efdefaa571fe73d6917e69c6fdc5
mohbuang09-art <mohbuang09@gmail.com>
2026-07-30 09:41:41 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-console.log("Tikfinity Avatar Ball siap.");
+const overlay = document.getElementById("overlay");
+
+const bola = document.createElement("div");
+bola.className = "avatar";
+
+bola.style.left = "100px";
+bola.style.top = "100px";
+
+bola.innerHTML = `
+<img src="https://placehold.co/200x200/png">
+<div class="username">Tes Overlay</div>
+`;
+
+overlay.appendChild(bola);
